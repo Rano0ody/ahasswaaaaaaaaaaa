a666e04efb4b2833c78c8365fc71872005dbbfd5
Rano0ody <rand45042@gmail.com>
2024-10-29 15:36:16 +0300

    rand

---
+++ ahasswaaaaaaaaaaa/Main.swift
@@ -0,0 +1,18 @@
+//
+//  Main.swift
+//  ahasswaaaaaaaaaaa
+//
+//  Created by Rand abdullatif on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+struct Main: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Main()
+}
