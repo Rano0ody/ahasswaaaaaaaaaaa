a282fa650fc7de04d2b1cc96ea2a35ceb6738d24
Rano0ody <rand45042@gmail.com>
2024-10-29 14:47:21 +0300

    Initial Commit

---
+++ ahasswaaaaaaaaaaa/ahasswaaaaaaaaaaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ahasswaaaaaaaaaaaApp.swift
+//  ahasswaaaaaaaaaaa
+//
+//  Created by Rand abdullatif on 26/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct ahasswaaaaaaaaaaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ahasswaaaaaaaaaaaTests/ahasswaaaaaaaaaaaTests.swift
@@ -0,0 +1,17 @@
+//
+//  ahasswaaaaaaaaaaaTests.swift
+//  ahasswaaaaaaaaaaaTests
+//
+//  Created by Rand abdullatif on 26/04/1446 AH.
+//
+
+import Testing
+@testable import ahasswaaaaaaaaaaa
+
+struct ahasswaaaaaaaaaaaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
